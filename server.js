3051b16cf2f9a28407d6ead80870ec18e0b35d7e
Sayan Roy <rsayan553@gmail.com>
2021-03-17 23:50:34 +0530

    add cors

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-// Cors 
-// const corsOptions = {
-//   origin: process.env.ALLOWED_CLIENTS.split(',')
-//   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-// }
+// Cors
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(','),
+};
 
 // Default configuration looks like
 // {
@@ -18,7 +17,7 @@ const cors = require('cors');
 //     "optionsSuccessStatus": 204
 //   }
 
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
@@ -29,10 +28,9 @@ app.use(express.json());
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-// Routes 
+// Routes
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
-
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
