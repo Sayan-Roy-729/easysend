9030fbca6da2ade649530625e6d0dadda623ddc2
Sayan Roy <rsayan553@gmail.com>
2021-03-18 16:01:09 +0530

    bug fixed

--- services/emailService.js
+++ services/emailService.js
@@ -8,7 +8,7 @@ module.exports = async ({ from, to, subject, text, html }) => {
     sendgridTransport({
       auth: {
         api_key:
-          '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',
+          process.env.SENDGRID_API_KEY,
       },
     })
   );
